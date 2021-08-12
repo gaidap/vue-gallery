5f4cac924ca5b411d756b8cb91121c60dbd81b13
Paul Gaida <pc.gaida@gmail.com>
2021-08-12 22:48:43 +0200

    connected vuex to vue. setup vuex module. implemented logout.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import App from './App';
 import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+
+Vue.config.productionTip = false;
 
 new Vue({
-    render: h => h(App)
+    store,
+    render: h => h(App),
 }).$mount('#app');
